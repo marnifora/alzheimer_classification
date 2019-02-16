55b31fcb19e2292695cbed65286dc34d9db94e34
marnifora <marnifora@gmail.com>
2019-02-16 11:35:41 +0100

    Little corrections in plink_processing.sh.

--- plink_processing.sh
+++ plink_processing.sh
@@ -52,30 +52,34 @@ done
 # convert binary to text format
 if [[ ${binary} -eq 1 ]]; then
     ${plinkdir}plink-1.9/plink --bfile ${indir}${plink} --recode --not-chr 0 --out ${indir}${plink}
-    echo Convertion of binary plink files into text format done!
+    echo "Convertion of binary plink files into text format done!"
 fi
 
 # sort SNPs in dbsnp and in map file
 if [[ ${sort} -eq 1 ]]; then
-    echo Sorting has just began!
+    echo "Sorting has just began!"
     export LC_ALL=C
-    sort -k1 ${dbsnp} >${dbsnp/./_ascii.}
+    if [[ ! -f ${dbsnp/./_ascii.} ]]; then
+        echo "Sorting dbsnp!"
+        sort -k1 ${dbsnp} >${dbsnp/./_ascii.}
+    fi
+    echo "Sorting map file!"
     sort -k2 ${indir}${plink}".map" >${indir}${plink}"_ascii.map"
-    echo Files were sorted succesfully!
+    echo "File(s) were sorted successfully!"
 fi
 
 # compare SNPs with reference, make pid_chr.txt and genome_stats.txt, filter plink files
 if [[ ${step1} -eq 1 ]]; then
-    echo Step one has just began!
+    echo "Step one has just began!"
     python3 plink_step_one.py -indir ${indir} -outdir ${outdir} -plink ${plink} -dbsnp ${dbsnp}
-    echo Step one done!
+    echo "Step one done!"
     ${plinkdir}plink-1.9/plink --file ${indir}${plink} --exclude ${outdir}"missing_snps_ref.txt" --recode --out ${indir}${plink}"_filtered"
-    echo SNPs were excluded, filtered plink files have been made.
+    echo "SNPs were excluded, filtered plink files have been made."
 fi
 
 # make files with matrices and snps lists
 if [[ ${step2} -eq 1 ]]; then
-    echo Step two has just began!
+    echo "Step two has just began!"
     python3 plink_step_two.py -indir ${indir} -outdir ${outdir} -plink ${plink}"_filtered"
-    echo Step two done!
+    echo "Step two done!"
 fi
